5215d5b28be21466a9d594fed9badb607695acd7
CKBhalaji <111473426+CKBhalaji@users.noreply.github.com>
2023-05-01 11:36:42 +0530

    Add files via upload

---
+++ Ex-14-Web Srevices/customer.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author bhala
+ */
+public class customer {
+    private int id;
+    private String name;
+    private String email;
+    private String phone;
+
+    public customer() {}
+
+    public customer(int id, String name, String email, String phone) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    // getter and setter methods
+}
