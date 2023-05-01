7932459acfb9d37bc50ebb4ac5dbc84375d3bbae
CKBhalaji <111473426+CKBhalaji@users.noreply.github.com>
2023-05-01 17:57:02 +0530

    Update customer.java

--- Ex-14-Web Srevices/customer.java
+++ Ex-14-Web Srevices/customer.java
@@ -8,20 +8,49 @@
  *
  * @author bhala
  */
-public class customer {
-    private int id;
+package Ex_14_IP;
+class Customer
+{
+   private int id;
     private String name;
+    private String address;
     private String email;
-    private String phone;
-
-    public customer() {}
-
-    public customer(int id, String name, String email, String phone) {
+    public Customer(int id, String name, String address, String email) 
+    {
         this.id = id;
-        this.name = name;
+        this.name=name;
+        this.address=address;
         this.email = email;
-        this.phone = phone;
+        
     }
-
     // getter and setter methods
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String get_name() {
+        return name;
+    }
+    public void setname(String name) {
+        this.name = name;
+    }
+    public String get_address() {
+        return address;
+    }
+    public void set_address(String address) {
+        this.address = address;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    @Override
+    public String toString()
+    {
+        return "\nId:"+id+" Name:"+name+"Address "+address+" Email:"+email;
+    }
 }
